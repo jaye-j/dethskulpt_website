2e0fda893664a7c5622925422074fc54b8c9e188
jaye-j <jacobrobertjensen@gmail.com>
2020-03-14 23:30:52 -0500

    added carousel

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use(require('./controllers/'));
+app.set('view engine', 'ejs');
+
+
+app.use(require('./controllers/index.js'));
 
 app.listen(3000, () => {
     console.log('Listening on 3000');
