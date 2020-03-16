ad93e151a6c1c118d703d0ccfde8c3b14b061926
jaye-j <jacobrobertjensen@gmail.com>
2020-03-15 19:34:19 -0500

    started mugs ejs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(require('./controllers/index.js'));
+app.use(require('./controllers/mugs.js'));
 
 app.listen(3000, () => {
     console.log('Listening on 3000');
